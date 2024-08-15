93973908bfa7102ff0b4505f0161c90017a801cf
mrbonk97 <mrbonk97@gmail.com>
2024-08-15 18:40:26 +0900

    빌드 오류 해결 테스트중4

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,17 @@ export default function Home() {
           intensity={Math.PI}
         />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        {/* 우측 위 */}
+        <Bicycle3d1 x={4} y={2} z={1 + z} rotateZ={2} rotateX={1} />
+        {/* 왼쪽 중단 */}
+        <Bicycle3d2 x={-4} y={0} z={1 + z} rotateY={0.5} rotateZ={1} />
+        {/* 하단 가운데 */}
+        <Bicycle3d3 x={1} y={-2} z={1 + z} rotateX={0} rotateY={0.5} />
+        {/* 좌측 하단 */}
+        <Bicycle3d1 x={-2} y={-1} z={1.5 + z} />
+        {/* 중간쯤 오른쪽 멀리 */}
+        <Bicycle3d2 x={2} y={0} z={0 + z} rotateY={-0.2} rotateZ={-0.2} />
+        <Bicycle3d3 x={-2} y={0.5} z={-2 + z} />
       </Canvas>
     </main>
   );
