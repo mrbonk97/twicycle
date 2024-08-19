1320e7d82f3ef21ec661af78ba38de5f86c8e1cb
mrbonk97 <mrbonk97@gmail.com>
2024-08-19 17:55:13 +0900

    404 페이지 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const Notfound = () => {
+  return (
+    <main className="h-full flex2 flex-col gap-5 text-blue-400">
+      <h1 className="text-5xl font-black">404 ERROR</h1>
+      <h2 className="text-lg font-medium">
+        요청하신 페이지를 찾을 수 없습니다.
+      </h2>
+      <Button variant={"link"} asChild>
+        <Link href={"/main"}>메인으로 돌아가기</Link>
+      </Button>
+    </main>
+  );
+};
+
+export default Notfound;
